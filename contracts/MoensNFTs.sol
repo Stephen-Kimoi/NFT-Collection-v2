813b117f6d0f1df15b49d4278224dc568e4f2416
Steve Kimoi <moenisteve@gmail.com>
2022-12-09 07:51:05 +0300

    creating the contract

---
+++ contracts/MoensNFTs.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract MoensNFT {
+    constructor(){
+        console.log("I am about to create some NFTs!!"); 
+    }
+}
