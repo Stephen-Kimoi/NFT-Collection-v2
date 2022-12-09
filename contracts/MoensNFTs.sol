bf3a478129fb10097a3c5575aa5a986b0ca163e0
Steve Kimoi <moenisteve@gmail.com>
2022-12-09 09:50:11 +0300

    Mint NFT function

--- contracts/MoensNFTs.sol
+++ contracts/MoensNFTs.sol
@@ -2,10 +2,35 @@
 
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MoensNFT {
-    constructor(){
+import "hardhat/console.sol";
+
+abstract contract MoensNFT is ERC721 {
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MoensNFT", "MFT"){
         console.log("I am about to create some NFTs!!"); 
     }
+
+    function mintNft() public {
+       uint256 itemId = _tokenIds.current(); 
+       
+       _safeMint(msg.sender, itemId); 
+
+       tokenURI(itemId); 
+
+       _tokenIds.increment(); 
+
+       console.log("An NFT with ID of %s has been minted to %s", itemId, msg.sender); 
+       
+    } 
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory){
+       require(_exists(_tokenId)); 
+       return " https://jsonkeeper.com/b/LGJK"; 
+    } 
 }
