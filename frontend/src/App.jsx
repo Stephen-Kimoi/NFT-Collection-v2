7075ea65c2fb4ccc18ac88b5617541d3c9a7a8cd
Steve Kimoi <moenisteve@gmail.com>
2022-12-16 10:20:20 +0300

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,63 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); 
+
+  const [walletConnected, setWalletConnected] = useState(false); 
+
+  // Connect wallet
+  const connectWallet = async () => {
+    console.log("Connecting wallet...")
+    try {
+      const { ethereum } = window; 
+
+    
+      const accounts = await ethereum.request({method: "eth_requestAccounts"}); 
+
+      const chainId = await ethereum.chainId; 
+
+    if(chainId != 5){
+      console.log("Kindly switch to metamask!"); 
+    }
+    
+    console.log("Wallet connected successully..."); 
+    setWalletConnected(true); 
+    return accounts[0]; 
+    } catch (error) {
+      console.error(error); 
+    }
+    
+  }
+
+  const getProviderOrSigner = async (needSigner) => {
+    const { ethereum } = window; 
+    
+    let signer; 
+    let provider; 
+
+    if(ethereum) {
+      provider = new ethers.providers.Web3Provider(ethereum); 
+
+      if(needSigner){
+        signer = provider.getSigner(); 
+        return signer; 
+      } else {
+        return provider; 
+      }
+    }
+  }
+
+  
+  // Render Methods
+  // const renderNotConnectedContainer = () => (
+  //   <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+  //     Connect to Wallet
+  //   </button>
+  // );
+
+
 
   return (
     <div className="App">
@@ -17,9 +71,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {
+          !walletConnected && (
+            <button onClick={connectWallet}>
+              Connect wallet
+            </button>
+          )
+        } 
+        {
+          walletConnected && (
+            <button onClick={ () => console.log("Mint an NFT!")}>
+              Mint an NFT
+            </button>
+          )
+        }
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
