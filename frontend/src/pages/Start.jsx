7760aacf9de9424f963997cdf1de06c86a12cfba
Steve Kimoi <moenisteve@gmail.com>
2022-12-21 00:07:51 +0300

    finishing the start page

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/Start.css'
 
-const Start = ({ metamaskInstalled, goerliNetwork }) => {
+const Start = (props) => {
   return (
     <div className='start-container'>
 
@@ -10,29 +10,35 @@ const Start = ({ metamaskInstalled, goerliNetwork }) => {
      </div>
 
      <div className='start-intro'>
-       <img src="./vite.svg" />
+       <img src="./intro.png" />
        <p>Hey there!<br/> Welcome to Moens NFT collection.<br/>Kindly follow the prompts.</p>
      </div>
 
-     <div className='start-prompts'>
-       {
-           !metamaskInstalled && (
-            <p>
-                Kindly install Metamask to continue using the application.<br/>
-                You can check it out over <a className="metamask-link" href='https://metamask.io/'>here</a>
-            </p>
-           )
-       }
-       {
-           !goerliNetwork && (
-               <p>
-                   Kindly switch to goerli network provider. 
-                   You can check out the instructions over <a className="metamask-link" href='https://metamask.io/'>here</a>
-               </p>
-           )
-       }
-    
-     </div>
+     {
+        props.metamaskInstalled === true && props.goerliNetwork === true ? (
+        <button className="button-connect" onClick={props.connectWallet}>
+          Connect wallet
+        </button>
+        ) : 
+        <div className='start-prompts'>
+            {
+                !props.metamaskInstalled && (
+                  <p>
+                      Kindly install Metamask to continue using the application.<br/>
+                      You can check it out over <a className="metamask-link" target="blank" href='https://metamask.io/'>here</a>
+                  </p>
+                )
+            }
+            {
+                !props.goerliNetwork && (
+                    <p>
+                        Kindly switch to goerli network provider to continue using the application. 
+                        You can check out the instructions over <a className="metamask-link" target="blank" href='https://blog.cryptostars.is/goerli-g%C3%B6rli-testnet-network-to-metamask-and-receiving-test-ethereum-in-less-than-2-min-de13e6fe5677#:~:text=Step%201%3A%20Log%20in%20to%20your%20Metamask%20wallet%20and%20click%20on%20the%20dropdown%20of%20networks%3A'>here</a>
+                    </p>
+                )
+            }
+        </div>
+     }
     
     </div>
   )
